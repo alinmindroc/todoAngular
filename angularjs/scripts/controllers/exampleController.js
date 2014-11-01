f38b285d7220b76627b035c1bb0b88f42c74daa6
Alin Mindroc <mindroc.alin@gmail.com>
2014-11-01 16:31:13 +0200

    working example

--- angularjs/scripts/controllers/exampleController.js
+++ angularjs/scripts/controllers/exampleController.js
@@ -1,4 +1,29 @@
 angular.module('example')
-    .controller('ctrl1', function ctrl1($scope, $routeParams, $filter) {
+    .controller('ctrl1', function ctrl1($scope) {
         'use strict';
+
+        $scope.editing = false;
+        $scope.var1 = "foo";
+        $scope.arr = [
+            {name: "learn angular", done: true},
+            {name: "go to webdev", done: true},
+            {name: "buy pizza", done: false},
+            {name: "eat pizza", done: false}
+        ];
+
+        $scope.add = function (item) {
+            $scope.arr.push({name: item, done: false});
+        };
+
+        $scope.edit = function (item, text) {
+            $scope.arr[$scope.arr.indexOf(item)].name = text;
+       };
+
+        $scope.remove = function (item) {
+            $scope.arr.splice($scope.arr.indexOf(item), 1);
+        };
+
+        $scope.switch = function (item) {
+            item.done = !item.done;
+        };
     });
